23dfde97636404c7a3ee56c5061c7b619a515529
jerry-unitmi <jerry.unitmi@gmail.com>
2024-10-06 20:14:25 +0900

    Refactor: 아이콘 활성 여부 결정 로직

--- background.js
+++ background.js
@@ -1,88 +1,98 @@
-function checkUrlAndView(url) {
-  const urlObj = new URL(url);
-  const pathSegments = urlObj.pathname.split('/');
+chrome.action.onClicked.addListener(async (tab) => {
+  try {
+    const isQueryBuilder = checkQueryBuilder(tab)
 
-  // Check if URL matches the required pattern
-  if ((
-    pathSegments.length > 4 &&
-    pathSegments[1] === 'project' &&
-    pathSegments[3] === 'firestore' &&
-    pathSegments[4] === 'databases'
-  ) || (
-      pathSegments.length > 6 &&
-      pathSegments[3] === 'project' &&
-      pathSegments[5] === 'firestore' &&
-      pathSegments[6] === 'databases'
-    )) {
-    const viewParam = urlObj.searchParams.get("view");
-    return viewParam;
-  }
-  return null;
-}
-
-function updateIcon(tabId, viewParam) {
-  let iconPath = {
-    "16": "icons/inactive_icon16.png",
-    "48": "icons/inactive_icon48.png",
-    "128": "icons/inactive_icon128.png"
-  };
-  let title = '[Firexport] Cannot export here';
-
-  if (viewParam === "query-view") {
-    iconPath = {
-      "16": "icons/active_icon16.png",
-      "48": "icons/active_icon48.png",
-      "128": "icons/active_icon128.png"
-    };
-    title = '[Firexport] Click to export'
-  }
-
-  chrome.action.setIcon({
-    tabId: tabId,
-    path: iconPath
-  });
-  chrome.action.setTitle({ title: title });
-}
-
-function handleIconClick(tab) {
-  if (tab.url) {
-    const viewParam = checkUrlAndView(tab.url);
-
-    if (viewParam === "query-view") {
-      // Execute the content script
-      chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        files: ['content.js']
-      });
-    } else {
-      console.log("Content script not executed: view is not 'query-view'");
+    if (!isQueryBuilder) {
+      return
     }
-  } else {
-    console.log("Content script not executed: URL does not match the required pattern");
+
+    await chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      files: ['content.js'],
+    })
+
+  } catch (e) {
+    console.log(e)
+  }
+})
+
+/**
+ * 어떠한 상황을 인식해야 하는가?
+ * - 새탭 열기
+ * - 탭 안에서 페이지 이동
+ * - 탭 이동
+ * 
+ * "새탭 열기" 관련 이벤트
+ * - onActivated
+ * - onUpdated
+ * 
+ * "탭 안에서 페이지 이동" 관련 이벤트
+ * - onUpdated
+ * 
+ * "탭 이동" 관련 이벤트
+ * - onActivated
+ */
+
+// 새탭 열기, 탭 안에서 페이지 이동
+chrome.tabs.onUpdated.addListener(async (_, changeInfo, tab) => {
+  if (changeInfo.status !== 'complete') {
+    return
+  }
+
+  await handleTabEvent(tab)
+})
+
+// 새탭 열기, 탭 이동
+chrome.tabs.onActivated.addListener(async (activeInfo) => {
+  const tab = await chrome.tabs.get(activeInfo.tabId)
+
+  await handleTabEvent(tab)
+})
+
+const inactiveIconPath = {
+  "16": "icons/inactive_icon16.png",
+  "48": "icons/inactive_icon48.png",
+  "128": "icons/inactive_icon128.png"
+}
+const activeIconPath = {
+  "16": "icons/active_icon16.png",
+  "48": "icons/active_icon48.png",
+  "128": "icons/active_icon128.png"
+}
+
+const inactiveTitle = 'Firexport - Cannot export here'
+const activeTitle = 'Firexport - Click to export'
+
+async function handleTabEvent(tab) {
+  try {
+    const isQueryBuilder = checkQueryBuilder(tab)
+
+    await Promise.all([
+      // isQueryBuilder ? chrome.action.enable(tab.id) : chrome.action.disable(tab.id),
+      chrome.action.setIcon({ path: isQueryBuilder ? activeIconPath : inactiveIconPath, tabId: tab.id }),
+      chrome.action.setTitle({ title: isQueryBuilder ? activeTitle : inactiveTitle, tabId: tab.id }),
+    ])
+
+  } catch (e) {
+    console.log(e)
   }
 }
 
-chrome.action.onClicked.addListener((tab) => {
-  if (tab) {
-    handleIconClick(tab);
+function checkQueryBuilder(tab) {
+  if (!tab.url) {
+    // tab.url이 없다는 것은 host_permission에 없다는 것이니 쿼리 빌더가 아니라는 뜻
+    return false
   }
-});
 
-function updateIconBasedOnURL(tabId, url) {
-  const viewParam = checkUrlAndView(url);
-  updateIcon(tabId, viewParam);
+  const urlObj = new URL(tab.url)
+
+  const host = urlObj.host
+  const pathSegments = urlObj.pathname.split('/')
+  const viewParam = urlObj.searchParams.get('view')
+
+  const isFirebaseConsole = host === 'console.firebase.google.com'
+  const isFirestoreDataTab = ['project', 'firestore', 'databases', 'data'].every((requiredPathSegment) => pathSegments.includes(requiredPathSegment))
+  const isQueryView = viewParam === 'query-view'
+
+  return isFirebaseConsole && isFirestoreDataTab && isQueryView
 }
-
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === 'complete' && tab.url) {
-    updateIconBasedOnURL(tabId, tab.url);
-  }
-});
-
-chrome.tabs.onActivated.addListener(activeInfo => {
-  chrome.tabs.get(activeInfo.tabId, tab => {
-    if (tab.url) {
-      updateIconBasedOnURL(activeInfo.tabId, tab.url);
-    }
-  });
-});
